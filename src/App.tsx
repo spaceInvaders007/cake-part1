2b8bd0d2b1afc65c5ffb889c7bed8b36c48e76df
Ignacio <ignacio.senese@abx.com>
2024-12-15 12:07:09 +1030

    Checking that theme changing functionality works before adding modal

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,39 @@
 import cakeLogo from "./assets/cake.svg";
 import "./App.css";
 import Switch from "@mui/material/Switch";
+import { useState } from "react";
 
-const label = { inputProps: { "aria-label": "Switch demo" } };
+const label = { inputProps: { "aria-label": "Switch theme" } };
 
 function App() {
+  const [theme, setTheme] = useState("light");
+
+  const toggleTheme = () => {
+    const newTheme = theme === "light" ? "dark" : "light";
+    setTheme(newTheme);
+    document.body.setAttribute("data-theme", newTheme);
+  };
+  
   return (
     <>
       <div className="header">
-        <img src={cakeLogo} alt="cake logo" />
+        <img src={cakeLogo} alt="cake logo" className="cake-logo"/>
       </div>
       <h2 className="welcome-heading">Welcome</h2>
       <div className="content-container">
         <h5>Change the mood</h5>
-        <p style={{ textAlign: "left" }}>
+        <p className="content-desc">
           Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non mi eu
           massa imperdiet aliquam.
         </p>
         <div className="switch-container">
           <span className="theme-label">Light</span>
-          <Switch {...label} defaultChecked />
+          <Switch
+            {...label}
+            defaultChecked
+            checked={theme === "dark"}
+            onChange={toggleTheme}
+          />
           <span className="theme-label">Dark</span>
         </div>
       </div>
